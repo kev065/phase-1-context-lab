1781269d5928dab552f1a19eae84878bba5534be
KELVIN MURITHI <kelvinmurithi@live.com>
2023-10-09 10:47:21 +0300

    commit 2

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ function createEmployeeRecord (array){
    return employee 
 }
 
+function createEmployeeRecords (array){
+    return array.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent (date){
+    const timeIn = {
+        type: "TimeIn",
+        hour: parseInt(date.slice(11)),
+        date: date.slice(0, 10)
+    }
+     this.timeInEvents.push(timeIn)
+    
+    return this
+}
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
